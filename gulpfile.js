f8da2f562d0c39b18c50dade8f72c51ec62f2610
Jay Shirley <git@shirley.im>
2015-01-15 08:57:09 -0800

    Adding in stylesheets and JS for server view.

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,30 @@
 var gulp = require('gulp');
+var del = require('del');
+var rename = require('gulp-rename');
 var source = require('vinyl-source-stream');
 var browserify = require('browserify');
+var sass = require('gulp-sass')
+var sourcemaps = require('gulp-sourcemaps');
+var minifyCSS = require('gulp-minify-css');
 
 gulp.task('watch', function(){
-    gulp.watch(["./public/**/*.jsx", "!./public/javascript/bundle.js"], ["browserify"]);
+  gulp.watch(["./public/javascript/**/*.jsx", "!./public/dist/*"], ["browserify"]);
+  gulp.watch(["./public/stylesheets/**/*.scss", "!./public/dist/*"], ["sass"]);
+});
+
+gulp.task('clean', function(cb) {
+  del(['./public/dist'], cb);
+});
+
+gulp.task('sass', ['clean'], function() {
+  gulp.src('./public/stylesheets/main.scss')
+  .pipe(sourcemaps.init())
+  .pipe(sass())
+  .pipe(sourcemaps.write())
+  .pipe(gulp.dest('./public/dist/'))
+  .pipe(minifyCSS())
+  .pipe(rename('main.min.css'))
+  .pipe(gulp.dest('./public/dist'));
 });
 
 gulp.task('browserify', function() {
@@ -11,5 +32,5 @@ gulp.task('browserify', function() {
       .transform('reactify')
       .bundle()
       .pipe(source('bundle.js'));
-      return bundleStream.pipe(gulp.dest('./public/javascript/dist'));
+      return bundleStream.pipe(gulp.dest('./public/dist'));
 });
