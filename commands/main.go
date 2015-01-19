86430bc08035f22d6a1a23fef2f6b2c7d7e5c6d0
Jay Shirley <git@shirley.im>
2015-01-18 19:54:12 -0800

    Just normalizing the day view to one method, makes more sense this way.

--- commands/main.go
+++ commands/main.go
@@ -1,7 +1,6 @@
 package commands
 
 import (
-	"fmt"
 	"github.com/jshirley/today/models"
 	"github.com/russross/blackfriday"
 	"github.com/wsxiaoys/terminal"
@@ -10,7 +9,7 @@ import (
 	//log "gopkg.in/inconshreveable/log15.v2"
 )
 
-func displayPlanningHints() {
+func DisplayReview() {
 	today := time.Now()
 	today_string := today.Format(time.RFC1123)
 
@@ -23,7 +22,7 @@ func displayPlanningHints() {
 	if len(entries) == 0 {
 		terminal.Stdout.Color("r").
 			Print("Yesterday was ", yesterday.Format(time.RFC1123), ".").Nl().
-			Print("- Nothing was planned yesterday, nothing to review today -")
+			Print("- Nothing was planned yesterday, nothing to review today -").Nl()
 	} else {
 		terminal.Stdout.Color("g").
 			Print("Review what's been done and what needs to be done:").Nl()
@@ -31,6 +30,16 @@ func displayPlanningHints() {
 			DisplayEntry(entry)
 		}
 	}
+
+	notes := models.NotesForToday()
+	if len(notes) > 0 {
+		terminal.Stdout.Color("y").
+			Print("Notes for ", today_string, ":").Nl()
+		for _, note := range notes {
+			DisplayNote(note)
+		}
+	}
+
 	terminal.Stdout.Nl().Reset()
 
 	//Reset().
@@ -53,24 +62,7 @@ func DisplayEntry(entry models.Entry) {
 }
 
 func DisplayNote(entry models.Note) {
-	fmt.Println(entry.Title)
-	fmt.Println(string(blackfriday.MarkdownBasic([]byte(entry.Note))))
-}
-
-func DisplayReview() {
-	entries := models.EntriesForToday()
-	for _, entry := range entries {
-		DisplayEntry(entry)
-	}
-
-	if len(entries) == 0 {
-		displayPlanningHints()
-	}
-
-	notes := models.NotesForToday()
-	if len(notes) > 0 {
-		for _, note := range notes {
-			DisplayNote(note)
-		}
-	}
+	terminal.Stdout.Color("w").
+		Print(entry.Title).Nl().
+		Print(string(blackfriday.MarkdownBasic([]byte(entry.Note)))).Nl()
 }
